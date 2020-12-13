3cd3a412e729303cb4c91de24e84d19e223415d6
Aaron Truong <plainaaron@gmail.com>
2020-12-12 18:49:08 -0800

    Implemented Email Verification

--- FitnessProgressTracker/app/src/main/java/com/example/fitnessprogresstracker/MainActivity.java
+++ FitnessProgressTracker/app/src/main/java/com/example/fitnessprogresstracker/MainActivity.java
@@ -78,8 +78,7 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
                     progressDialog.dismiss();
-                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                    checkEmailVerification();
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                     counter--;
@@ -91,5 +90,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void checkEmailVerification() {
+        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        Boolean emailflag = firebaseUser.isEmailVerified();
+
+        if(emailflag) {
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        } else {
+            Toast.makeText(this, "Verify your email", Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
     }
 }
